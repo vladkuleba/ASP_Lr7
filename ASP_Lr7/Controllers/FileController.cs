29bde081d73c6f3455f7c31d0dbb09f58622ea20
kuleb <kuleb@SPRIGGAN>
2024-03-18 04:17:11 +0100

    Add project files.

---
+++ ASP_Lr7/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+namespace ASP_Lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            var content = $"Name: {firstName}\nSurname: {lastName}";
+            var byteArray = Encoding.UTF8.GetBytes(content);
+            var stream = new MemoryStream(byteArray);
+
+            return File(stream, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
---
+++ ASP_Lr7/Views/File/DownloadFile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_Lr7.Views.File
+{
+    public class DownloadFileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
